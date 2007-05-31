c00bb0e4e1d9d33b73f2ad5c5adb5e3221ef2286
dmueller <dmueller@853679b2-d9fb-0310-bf1a-abebfb08175b>
2007-05-31 19:46:33 +0000

    make the check work
    git-svn-id: http://svn.suse.de/svn/desktop/trunk/rpmlint@6403 853679b2-d9fb-0310-bf1a-abebfb08175b

--- CheckPkgConfig.py
+++ CheckPkgConfig.py
@@ -1,4 +1,11 @@
 # vim:sw=4:et
+#---------------------------------------------------------------
+# Module          : rpmlint
+# File            : CheckExecDocs.py
+# Author          : Stephan Kulow, Dirk Mueller
+# Purpose         : Check for executable files in %doc
+#---------------------------------------------------------------
+
 from Filter import *
 import AbstractCheck
 import rpm
@@ -9,11 +16,14 @@ import os
 
 class PkgConfigCheck(AbstractCheck.AbstractFilesCheck):
     def __init__(self):
-        AbstractCheck.AbstractFilesCheck.__init__(self, "PkgConfigCheck", ".*/pkgconfig/.*\.pc")
-        self.rpm_build_dir=re.compile('/usr/src/packages/BUILD')
+        AbstractCheck.AbstractFilesCheck.__init__(self, "PkgConfigCheck", ".*/pkgconfig/.*\.pc$")
+        self.suspicious_dir=re.compile('^(?:/usr/src/[^\/]+/BUILD|/var|/tmp|/home)')
 
     def check_file(self, pkg, filename):
-        if pkg.grep(self.rpm_build_dir, filename):
+        if pkg.isSource():
+            return
+
+        if pkg.grep(self.suspicious_dir, filename):
             printError(pkg, "invalid-pkgconfig-file", filename)
 
 check=PkgConfigCheck()
