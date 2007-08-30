32e6961357fc1cfca8801d570514f8dd950eabdd
dmueller <dmueller@853679b2-d9fb-0310-bf1a-abebfb08175b>
2007-08-30 16:47:21 +0000

    fix false positive
    git-svn-id: http://svn.suse.de/svn/desktop/trunk/rpmlint@6528 853679b2-d9fb-0310-bf1a-abebfb08175b

--- CheckBuildRoot.py
+++ CheckBuildRoot.py
@@ -18,7 +18,7 @@ import stat
 class BuildRootCheck(AbstractCheck.AbstractFilesCheck):
     def __init__(self):
         AbstractCheck.AbstractFilesCheck.__init__(self, "BuildRootCheck", ".*")
-        self.build_root_re = re.compile('/var/tmp/[\w\!-\.]{1,60}-build')
+        self.build_root_re = re.compile('/var/tmp/[\w\!-\.]{1,60}-build/')
 
     def check_file(self, pkg, filename):
         if filename.startswith('/usr/lib/debug') or pkg.isSource():
