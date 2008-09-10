5901eebbb91693caa6197d48dc61b0630fd12f38
dmueller <dmueller@853679b2-d9fb-0310-bf1a-abebfb08175b>
2008-09-10 12:42:23 +0000

    check for wrong references to lib dirs
    git-svn-id: http://svn.suse.de/svn/desktop/trunk/rpmlint@6817 853679b2-d9fb-0310-bf1a-abebfb08175b

--- CheckPkgConfig.py
+++ CheckPkgConfig.py
@@ -21,12 +21,27 @@ class PkgConfigCheck(AbstractCheck.AbstractFilesCheck):
         #self.suspicious_dir=re.compile('(?:/usr/src/\w+/BUILD|/var/tmp|/tmp|/home|\@\w{1,50}\@)')
         self.suspicious_dir=re.compile('(?:/usr/src/\w+/BUILD|/var/tmp|/tmp|/home)')
 
+    def check(self, pkg):
+        # check for references to /lib when in lib64 mode
+        if pkg.arch in ('x86_64', 'ppc64', 's390x'):
+            self.wronglib_dir=re.compile('-L/usr/lib\\b')
+        else:
+            self.wronglib_dir=re.compile('-L/usr/lib64\\b')
+
+        AbstractCheck.AbstractFilesCheck.check(self, pkg)
+
+
     def check_file(self, pkg, filename):
         if pkg.isSource():
             return
 
         if pkg.grep(self.suspicious_dir, filename):
             printError(pkg, "invalid-pkgconfig-file", filename)
+
+        pc_file=file(pkg.dirName() + "/" + filename, "r")
+        for l in pc_file:
+            if l.startswith('Libs:') and self.wronglib_dir.search(l):
+                printError(pkg, 'pkgconfig-invalid-libs-dir', filename, l)
 
 check=PkgConfigCheck()
 
@@ -39,5 +54,9 @@ if Config.info:
 - it references invalid paths (e.g. /home or /tmp)
 
 Please double-check and report false positives.
-'''
+''',
+'pkgconfig-invalid-libs-dir',
+''' Your .pc file contains -L/usr/lib or -L/lib and is built for a lib64 target,
+or contains references to -L/usr/lib64 or -L/lib64 and is built for a lib target.
+Please remove the wrong library paths from the pc file.'''
 )
