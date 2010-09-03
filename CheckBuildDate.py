3b2b1dec1cc9c6aacd04e85606e045af2eb0d686
Cristian Rodríguez <cristian.rodriguez@opensuse.org>
2010-09-03 13:28:11 -0400

    Improve it so it actually works...

--- CheckBuildDate.py
+++ CheckBuildDate.py
@@ -1,4 +1,3 @@
-# vim:sw=4:et
 #############################################################################
 # File          : CheckBuilDate.py
 # Package       : rpmlint
@@ -19,25 +18,25 @@ import time
 class BuildDateCheck(AbstractCheck.AbstractFilesCheck):
     def __init__(self):
         AbstractCheck.AbstractFilesCheck.__init__(self, "CheckBuildDate", ".*")
-        self.looksliketime = re.compile('\b(2[0-3]|[01]?[0-9]):([0-5]?[0-9]):([0-5]?[0-9])\b')
-        self.istoday = re.compile('\b' + time.strftime("%b %d %Y") + '\b')
+        self.looksliketime = re.compile('(2[0-3]|[01]?[0-9]):([0-5]?[0-9]):([0-5]?[0-9])')
+        self.istoday = re.compile(time.strftime("%b %e %Y"))
 
     def check_file(self, pkg, filename):
         if filename.startswith('/usr/lib/debug') or pkg.isSource():
             return
 
-        if not stat.S_ISREG(pkg.files()[filename][0]):
+        if not stat.S_ISREG(pkg.files()[filename].mode):
             return
-		
+
         grep_date = pkg.grep(self.istoday, filename)
 
         if len(grep_date):
-            printWarning(pkg, "file-contains-current-date", filename, grep_date)
-
+            printWarning(pkg, "file-contains-current-date", filename)
+			
         grep_time = pkg.grep(self.looksliketime, filename)
 
         if len(grep_date) and len(grep_time):
-            printError(pkg, "file-contains-build-and-time", filename, grep_time)
+            printError(pkg, "file-contains-date-and-time", filename)
 
 check=BuildDateCheck()
 
@@ -45,6 +44,6 @@ if Config.info:
     addDetails(
 'file-contains-current-date',
 """Your file contains the current date, this may cause the package to rebuild in excess.""",
-'file-contains-build-and-time',
-"""Your file uses C __DATE and __TIME__ this causes the package to rebuild when not needed"""
+'file-contains-date-and-time',
+"""Your file uses  __DATE and __TIME__ this causes the package to rebuild when not needed"""
 )
