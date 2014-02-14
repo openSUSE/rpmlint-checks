f5f9f1f97511bbbf0bce05fccb88155f65d79050
Dirk Mueller <dmueller@suse.com>
2014-02-14 12:51:22 +0100

    Cleanup for Python 3.x

--- ErlangCheck.py
+++ ErlangCheck.py
@@ -6,37 +6,41 @@
 # Purpose       : Check for erlang compiled files
 #############################################################################
 
-from Filter import *
 import AbstractCheck
-import rpm
-import re
-import os
-import commands
 import Config
-import stat
+import Filter
+import re
+import rpm
 
 from pybeam import BeamFile
 
+
 class ErlangCheck(AbstractCheck.AbstractFilesCheck):
     def __init__(self):
-        AbstractCheck.AbstractFilesCheck.__init__(self, "ErlangCheck", ".*?\.beam$")
-        build_dir = rpm.expandMacro("%_builddir") 
+        AbstractCheck.AbstractFilesCheck.__init__(
+            self, "ErlangCheck", ".*?\.beam$")
+        build_dir = rpm.expandMacro("%_builddir")
         self.source_re = re.compile(build_dir)
 
     def check_file(self, pkg, filename):
         beam = BeamFile(pkg.files()[filename].path)
         if 'debug_info' not in beam.compileinfo['options']:
-            printWarning(pkg, "beam-compiled-without-debug_info", filename)
+            Filter.printWarning(
+                pkg, "beam-compiled-without-debug_info", filename)
         if not self.source_re.match(beam.compileinfo['source'].value):
-            printWarning(pkg, "beam-was-not-recompiled", filename, beam.compileinfo['source'].value)
+            Filter.printWarning(
+                pkg, "beam-was-not-recompiled", filename,
+                beam.compileinfo['source'].value)
 
-check=ErlangCheck()
+check = ErlangCheck()
 
 if Config.info:
-    addDetails(
+    Filter.addDetails(
 'beam-compiled-without-debug_info',
-"Your beam file indicates that it doesn't contain debug_info. Please, make sure that you compile with +debug_info.",
+""""Your beam file indicates that it doesn't contain debug_info.
+Please, make sure that you compile with +debug_info.""",
 'beam-was-not-recompiled',
-"It seems that your beam file was not compiled by you, but was just copied in binary form to destination. Please, make sure that you really compile it from the sources.",
+"""It seems that your beam file was not compiled by you, but was
+just copied in binary form to destination. Please, make sure
+that you really compile it from the sources.""",
 )
-
