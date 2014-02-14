f65bb0abac46fa5ccebfc9c37978cf47a0a1047f
Dirk Mueller <dmueller@suse.com>
2014-02-14 12:24:30 +0100

    Cleanup for Python 3.x switch

--- CheckExecDocs.py
+++ CheckExecDocs.py
@@ -6,25 +6,22 @@
 # Purpose         : Check for executable files in %doc
 #---------------------------------------------------------------
 
-from Filter import *
 import AbstractCheck
-import rpm
-import re
-import commands
-import stat
 import Config
-import os
-import string
+import Filter
+import stat
+
 
 def ignore_pkg(name):
     if name.startswith('bundle-'):
-       return True
+        return True
     if name.find('-devel') != -1:
         return True
     if name.find('-doc') != -1:
         return True
 
     return False
+
 
 def lang_ignore_pkg(name):
     if ignore_pkg(name):
@@ -35,6 +32,7 @@ def lang_ignore_pkg(name):
         return True
 
     return False
+
 
 class ExecDocsCheck(AbstractCheck.AbstractCheck):
     def __init__(self):
@@ -47,51 +45,56 @@ class ExecDocsCheck(AbstractCheck.AbstractCheck):
             return
 
         files = pkg.files()
-        complete_size=0
-        lang_size=0
+        complete_size = 0
+        lang_size = 0
         for f, pkgfile in files.items():
             if stat.S_ISREG(pkgfile.mode):
                 complete_size += pkgfile.size
                 if pkgfile.lang != '':
                     lang_size += pkgfile.size
 
-        doc_size=0
+        doc_size = 0
         for f in pkg.docFiles():
             if stat.S_ISREG(files[f].mode):
                 doc_size += files[f].size
 
-        if doc_size * 2 >= complete_size \
-           and doc_size > 100*1024 and (complete_size - doc_size) * 20 > complete_size \
-           and not ignore_pkg(pkg.name):
-            printWarning(pkg, "package-with-huge-docs", ("%3d%%" % (doc_size * 100 / complete_size)) )
+        if doc_size * 2 >= complete_size and \
+           doc_size > 100*1024 and \
+           (complete_size - doc_size) * 20 > complete_size and \
+           not ignore_pkg(pkg.name):
+            Filter.printWarning(pkg, "package-with-huge-docs",
+                                ("%3d%%" % (doc_size * 100 / complete_size)))
 
         if lang_size * 2 >= complete_size \
-           and lang_size > 100*1024 and (complete_size - lang_size) * 20 > complete_size \
-           and not lang_ignore_pkg(pkg.name):
-            printWarning(pkg, "package-with-huge-translation", ("%3d%%" % (lang_size * 100 / complete_size)))
+           and lang_size > 100*1024 and \
+           (complete_size - lang_size) * 20 > complete_size and \
+           not lang_ignore_pkg(pkg.name):
+            Filter.printWarning(pkg, "package-with-huge-translation",
+                                ("%3d%%" % (lang_size * 100 / complete_size)))
 
         for f in pkg.docFiles():
-            mode=files[f].mode
-            if not stat.S_ISREG(mode) or not mode & 0111:
-               continue
-            for ext in ['txt', 'gif', 'jpg', 'html', 'pdf', 'ps', 'pdf.gz', 'ps.gz']:
-               if f.endswith("." + ext):
-                   printError(pkg, 'executable-docs', f)
+            mode = files[f].mode
+            if not stat.S_ISREG(mode) or not mode & 0o111:
+                continue
+            for ext in ['txt', 'gif', 'jpg', 'html',
+                        'pdf', 'ps', 'pdf.gz', 'ps.gz']:
+                if f.endswith("." + ext):
+                    Filter.printError(pkg, 'executable-docs', f)
 
             for name in ['README', 'NEWS', 'COPYING', 'AUTHORS']:
                 if f.endswith("/" + name):
-                    printError(pkg, 'executable-docs', f)
+                    Filter.printError(pkg, 'executable-docs', f)
 
-check=ExecDocsCheck()
+check = ExecDocsCheck()
 
 if Config.info:
-    addDetails(
-'executable-docs',
-"Documentation should not be executable.",
-'package-with-huge-docs',
-"""More than half the size of your package is documentation.
-Consider splitting it into a -doc subpackage.""",
-'package-with-huge-translation',
-"""More than half the size of your package is language-specific.
-Consider splitting it into a -lang subpackage."""
-)
+    Filter.addDetails(
+        'executable-docs',
+        "Documentation should not be executable.",
+        'package-with-huge-docs',
+        """More than half the size of your package is documentation.
+        Consider splitting it into a -doc subpackage.""",
+        'package-with-huge-translation',
+        """More than half the size of your package is language-specific.
+        Consider splitting it into a -lang subpackage."""
+    )
