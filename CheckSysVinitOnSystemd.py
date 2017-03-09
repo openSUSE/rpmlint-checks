c30a04c22a62e3f0d596ae78d02261d0020a50d6
Ludwig Nussel <ludwig.nussel@suse.de>
2017-03-09 16:35:05 +0100

    Fix sysv init/boot script detection
    Also change tag names and explanation

--- CheckSysVinitOnSystemd.py
+++ CheckSysVinitOnSystemd.py
@@ -14,9 +14,9 @@ import os
 import stat
 import Pkg
 
-insserv_tag = 'insserv-is-required-on-systemd-based'
-etcinit_tag = 'etc-init-found-on-systemd-based'
-bootscr_tag = 'sysv-boot-script-found-on-systemd-based'
+insserv_tag = 'suse-obsolete-insserv-requirement'
+etcinit_tag = 'suse-deprecated-init-script'
+bootscr_tag = 'suse-deprecated-boot-script'
 
 class CheckSysVinitOnSystemd(AbstractCheck.AbstractFilesCheck):
     def __init__(self):
@@ -34,31 +34,24 @@ class CheckSysVinitOnSystemd(AbstractCheck.AbstractFilesCheck):
         for fn, pkgfile in pkg.files().items():
             if not fn.startswith('/etc/init.d'):
                 continue
-            if stat.S_ISDIR(pkgfile.mode):
-                printError(pkg, etcinit_tag, fn)
-            else:
+            if os.path.basename(fn).startswith('boot.'):
                 printError(pkg, bootscr_tag, fn)
+            else:
+                printError(pkg, etcinit_tag, fn)
 
 check = CheckSysVinitOnSystemd()
-
-#
-# Should be set in the global configuration file
-#
-#Config.setBadness(insserv_tag, 10000)
-#Config.setBadness(etcinit_tag, 10000)
-#Config.setBadness(bootscr_tag, 10000)
 
 if Config.info:
     addDetails(
 insserv_tag,
-'''packages shall not require insserv on a system based on systemd
-therefor please consider to remove such dependencies''',
+'''In systemd based distributions insserv is obsolete.
+Please remove dependencies on insserv.''',
 etcinit_tag,
-'''packages shall not include /etc/init.d/ on a system based on systemd
-as this is deprecated, please remove''',
+'''SysV init scripts are deprecated. Please migrate to
+systemd service files.''',
 bootscr_tag,
-'''packages shall not install SysVinit boot scripts on a system based on systemd
-therefor please convert all boot scripts into systemd unit files''',
+'''SysV boot scripts are deprecated. Please migrate to
+systemd service files.''',
 )
 
 # Local variables:
