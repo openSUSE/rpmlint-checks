3803f11cea43a4238b96c1a083c1582e5177c35b
Dirk Mueller <dmueller@suse.com>
2017-09-30 10:11:43 +0200

    Flake8 / Stop leaking filedescriptors

--- TmpFilesCheck.py
+++ TmpFilesCheck.py
@@ -1,20 +1,16 @@
-# -*- coding: utf-8 -*-
-#############################################################################
 # File          : TmpFilesCheck.py
 # Package       : rpmlint
 # Author        : Ludwig Nussel
 # Created on    : Wed Sep 03 10:36 2014
 # Purpose       : Check systemd created tmpfiles are included in filelist
-#############################################################################
 
-import os
 import re
 
-from Filter import addDetails, printError, printWarning
+from Filter import addDetails, printWarning
 import AbstractCheck
-import Pkg
 import stat
 import rpm
+
 
 class TmpFilesCheck(AbstractCheck.AbstractCheck):
     '''Check systemd created tmpfiles are included in filelist'''
@@ -42,38 +38,40 @@ class TmpFilesCheck(AbstractCheck.AbstractCheck):
                 printWarning(pkg, "tmpfile-not-regular-file", fn)
                 continue
 
-            pattern = re.compile(r'systemd-tmpfiles --create .*%s'%re.escape(fn))
+            pattern = re.compile(
+                r'systemd-tmpfiles --create .*%s' % re.escape(fn))
             if (not postin or not pattern.search(postin)) and \
                     (not prein or not pattern.search(prein)):
                 printWarning(pkg,
                              'postin-without-tmpfile-creation', fn)
 
-            for line in open(pkgfile.path):
-                # skip comments
-                line = line.split('#')[0].split('\n')[0]
-                line = line.lstrip()
-                if not len(line):
-                    continue
-                line = re.split(r'\s+', line)
-                # format is
-                #Type Path        Mode UID  GID  Age Argument
-                # we only need type and path
-                if len(line) < 3:
-                    continue
-                t = line[0]
-                p = line[1]
-                if t.endswith('!'):
-                    t = t[:-1]
-                if not t in interesting_types:
-                    continue
+            with open(pkgfile.path) as inputf:
+                for line in inputf:
+                    # skip comments
+                    line = line.split('#')[0].split('\n')[0]
+                    line = line.lstrip()
+                    if not len(line):
+                        continue
+                    line = re.split(r'\s+', line)
+                    # format is
+                    # Type Path        Mode UID  GID  Age Argument
+                    # we only need type and path
+                    if len(line) < 3:
+                        continue
+                    t = line[0]
+                    p = line[1]
+                    if t.endswith('!'):
+                        t = t[:-1]
+                    if t not in interesting_types:
+                        continue
 
-                tmp_files.add(p)
+                    tmp_files.add(p)
 
-                if not p in pkg.files():
-                    printWarning(pkg, "tmpfile-not-in-filelist", p)
-                    continue
-                if not pkg.files()[p].is_ghost:
-                    printWarning(pkg, "tmpfile-not-ghost", p)
+                    if p not in pkg.files():
+                        printWarning(pkg, "tmpfile-not-in-filelist", p)
+                        continue
+                    if not pkg.files()[p].is_ghost:
+                        printWarning(pkg, "tmpfile-not-ghost", p)
 
         # now check remaining ghost files that are not already
         # handled by systemd-tmpfiles
@@ -90,20 +88,24 @@ class TmpFilesCheck(AbstractCheck.AbstractCheck):
                                  'postin-without-ghost-file-creation', f)
 
 
-
 check = TmpFilesCheck()
 
 addDetails(
 'postin-without-ghost-file-creation',
 '''A file tagged as ghost is not created during %prein nor during %postin.''',
+
 'postin-without-tmpfile-creation',
-'''Please use the %tmpfiles_create macro in %post for each of your tmpfiles.d files''',
+'''Please use the %tmpfiles_create macro in %post for each of your
+tmpfiles.d files''',
+
 'tmpfile-not-regular-file',
-'''files in tmpfiles.d need to be regular files''', # otherwise we won't open it :-)
+'''files in tmpfiles.d need to be regular files''',
+
 'tmpfile-not-in-filelist',
 '''please add the specified file to your %files section as %ghost so
 users can easily query who created the file, it gets uninstalled on
 package removal and finally other rpmlint checks see it''',
+
 'tmpfile-not-ghost',
 '''the specified file is not marked as %ghost although created at
 runtime via tmpfiles mechanism.'''
