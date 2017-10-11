c90062ef30ec343a060331f1b867e9f30d2951bf
Dirk Mueller <dmueller@suse.com>
2017-10-11 10:02:36 +0200

    Python 3.x porting of the ErlangCheck

--- ErlangCheck.py
+++ ErlangCheck.py
@@ -7,8 +7,8 @@
 #############################################################################
 
 import AbstractCheck
-import Config
 import Filter
+import Pkg
 import re
 import rpm
 
@@ -27,7 +27,7 @@ class ErlangCheck(AbstractCheck.AbstractFilesCheck):
         if 'debug_info' not in beam.compileinfo['options']:
             Filter.printWarning(
                 pkg, "beam-compiled-without-debug_info", filename)
-        if not self.source_re.match(beam.compileinfo['source'].value):
+        if not self.source_re.match(Pkg.b2s(beam.compileinfo['source'].value)):
             Filter.printWarning(
                 pkg, "beam-was-not-recompiled", filename,
                 beam.compileinfo['source'].value)
@@ -35,11 +35,11 @@ class ErlangCheck(AbstractCheck.AbstractFilesCheck):
 
 check = ErlangCheck()
 
-if Config.info:
-    Filter.addDetails(
+Filter.addDetails(
 'beam-compiled-without-debug_info',
 """"Your beam file indicates that it doesn't contain debug_info.
 Please, make sure that you compile with +debug_info.""",
+
 'beam-was-not-recompiled',
 """It seems that your beam file was not compiled by you, but was
 just copied in binary form to destination. Please, make sure
